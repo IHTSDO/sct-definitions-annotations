d9794c7cde3bc83b9fcd4bab60adbc857592a941
Alejandro Lopez Osornio <alopez72@gmail.com>
2022-10-16 20:50:33 -0300

    Initial commit

---
+++ src/app/services/terminology.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TerminologyService {
+
+  snowstormFhirBase = 'https://snowstorm.ihtsdotools.org/snowstorm/snomed-ct/fhir';
+  defaultFhirUrlParam = 'http://snomed.info/sct'; // 'http://snomed.info/sct/11000221109/version/20211130'
+  fhirUrlParam = this.defaultFhirUrlParam;
+  lang = 'en';
+
+  constructor(private http: HttpClient) { }
+
+  setSnowstormFhirBase(url: string) {
+    this.snowstormFhirBase = url;
+  }
+  
+  setFhirUrlParam(url: string) {
+    this.fhirUrlParam = url;
+  }
+
+  setLang(lang: string) {
+    this.lang = lang;
+  }
+
+  getCodeSystems() {
+    let requestUrl = `${this.snowstormFhirBase}/CodeSystem`;
+    return this.http.get<any>(requestUrl)
+      .pipe(
+        catchError(this.handleError<any>('getCodeSystems', {}))
+      );
+  }
+
+  getValueSetExpansionUrl(ecl: string, terms: string, offset?: number, count?:number) {
+    if (!offset) offset = 0;
+    if (!count) count = 20;
+    return `${this.snowstormFhirBase}/ValueSet/$expand?url=${this.fhirUrlParam}?fhir_vs=ecl/${encodeURIComponent(ecl)}&count=${count}&offset=${offset}&includeDesignations=true&filter=${terms}&designation=${this.lang}`
+  }
+
+  expandValueSet(ecl: string, terms: string, offset?: number, count?:number): Observable<any> {
+    let requestUrl = this.getValueSetExpansionUrl(ecl, terms, offset, count);
+    return this.http.get<any>(requestUrl)
+      .pipe(
+        catchError(this.handleError<any>('expandValueSet', {}))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
